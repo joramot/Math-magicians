988ceaba9f967d6175f5bb50d289491aa803d9c5
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-09-20 20:04:05 -0600

    Implement click event handlers

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,61 @@
+import { useState } from 'react';
 import calculate from './logic/calculate';
 
-function Calculator() {
+const Calculator = () => {
+  const [inputObject, setInputObject] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const type = (buttonName) => {
+    const output = calculate(inputObject, buttonName);
+    setInputObject({
+      total: output.total,
+      next: output.next,
+      operation: output.operation,
+    });
+    // console.log(inputObject);
+  };
+
   return (
     <div>
       <div className="calculator-container">
-        <input className="screen" placeholder="0" />
+        <input className="screen" placeholder="0" value={`${inputObject.total ? inputObject.total : ''}${inputObject.operation ? inputObject.operation : ''}${inputObject.next ? inputObject.next : ''}`} />
         <div className="buttons-grid">
           <div className="row">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button className="orange" type="button">÷</button>
+            <button type="button" onClick={() => type('AC')}>AC</button>
+            <button type="button" onClick={() => type('+/-')}>+/-</button>
+            <button type="button" onClick={() => type(' % ')}>%</button>
+            <button className="orange" type="button" onClick={() => type(' ÷ ')}>÷</button>
           </div>
           <div className="row">
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button className="orange" type="button">x</button>
+            <button type="button" onClick={() => type('7')}>7</button>
+            <button type="button" onClick={() => type('8')}>8</button>
+            <button type="button" onClick={() => type('9')}>9</button>
+            <button className="orange" type="button" onClick={() => type(' x ')}>x</button>
           </div>
           <div className="row">
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button className="orange" type="button">-</button>
+            <button type="button" onClick={() => type('4')}>4</button>
+            <button type="button" onClick={() => type('5')}>5</button>
+            <button type="button" onClick={() => type('6')}>6</button>
+            <button className="orange" type="button" onClick={() => type(' - ')}>-</button>
           </div>
           <div className="row">
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button className="orange" type="button">+</button>
+            <button type="button" onClick={() => type('1')}>1</button>
+            <button type="button" onClick={() => type('2')}>2</button>
+            <button type="button" onClick={() => type('3')}>3</button>
+            <button className="orange" type="button" onClick={() => type(' + ')}>+</button>
           </div>
           <div className="row">
-            <button type="button" id="zero">0</button>
-            <button type="button">.</button>
-            <button className="orange" type="button">=</button>
+            <button type="button" id="zero" onClick={() => type('0')}>0</button>
+            <button type="button" onClick={() => type('.')}>.</button>
+            <button className="orange" type="button" onClick={() => type('=')}>=</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Calculator;
