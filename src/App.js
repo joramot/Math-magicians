9882543b4bd622375ecf6aaa9940599d06ef21d1
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-09-27 12:13:24 -0600

    Add one nav and three p elements and  add Home.js in the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 import Quotes from './components/logic/Quotes';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <div>
+          <h1> Math Magicians </h1>
+        </div>
+        <div className="">
+          <p>Home</p>
+          <p>Calculator</p>
+          <p>Quotes</p>
+        </div>
+      </nav>
+      <Home />
       <Quotes />
       <Calculator />
     </div>
